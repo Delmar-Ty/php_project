34f69d28130d20474f4e00f6082f4e5e584fa611
Delmar <89948545+Delmar-Ty@users.noreply.github.com>
2023-02-06 10:38:21 -0700

    Add files via upload

---
+++ order_food/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Account Login</title>
+
+    <link rel="stylesheet" href="assets/login.css">
+</head>
+<body>
+    
+    <div id="login-container">
+        <form action="post" id="form">
+            <input type="text" placeholder="Username" required class="form-input">
+            <input type="password" placeholder="Password" required class="form-input">
+
+            <div id="button-container">
+                <button type="submit" id="login-button">Log In</button>
+                <button id="signup-button">Sign Up</button>
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
