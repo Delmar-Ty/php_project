b377aa8994d45df07d4690de3a4b31f69b478567
Delmar <reliablesource13@gmail.com>
2023-02-08 15:14:52 -0700

    login works les go

--- login.php
+++ login.php
@@ -1,5 +1,20 @@
+<?php include 'db.php' ?>
+
 <?php 
     
-    header("Location: signup.php");
+    $username = $_POST['username'];
+
+    $userAuth = "SELECT * FROM users WHERE username = '{$username}'";
+
+    $result = mysqli_query($conn, $userAuth);
+    $user = $result -> fetch_assoc();
+
+    if (password_verify($_POST['password'], $user['pass'])) {
+        echo 'correct login';
+    } else {
+        echo 'incorrect login';
+    }
+
+    mysqli_free_result($result);
    
 ?>
--- signup.php
+++ signup.php
@@ -2,29 +2,21 @@
 
 <?php 
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$sql = "SELECT username FROM users WHERE username = $username";
-// echo mysqli_query($conn, $sql);
-$result = mysqli_query($conn, $sql);
+    $username = $_POST['username'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-if ($result) {
-    echo 'Username already exists';
-    mysqli_free_result($result);
-    sleep(5);
-    header('Location: login.html');
-} else {
-    mysqli_free_result($result);
-    $createUser = "INSERT INTO users (username, password, email, phone) VALUES ('$username', '$password', '$email', $phone)";
-    if (mysqli_query($conn, $createUser)) {
-        echo 'Account Created';
+    $checkUser = "SELECT * FROM users WHERE username = '{$username}'";
+    $result = mysqli_query($conn, $checkUser);
+
+    if ($result->num_rows == 0) {
+        mysqli_free_result($result);
+        $createUser = "INSERT INTO users (username, pass) VALUES ('{$username}','{$password}')";
+        mysqli_query($conn, $createUser);
         header('Location: login.html');
     } else {
-        echo 'Error';
+        mysqli_free_result($result);
+        echo 'Error, user already exists!';
+        sleep(5);
         header('Location: login.html');
     }
-}
-
 ?>
